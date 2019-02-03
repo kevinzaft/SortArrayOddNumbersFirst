861d52e77514880c6f6d6787290aeb8768d668a8
Kevin Huangfu <kevinzaft@gmail.com>
2019-02-03 18:59:31 -0500

    Add project files.

---
+++ CodeTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace CodeTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arry = new int[6] {1,2,3,4,5,6 };
+            arry = arry.GroupBy(x => x % 2 == 0).SelectMany(g => g).ToArray();
+        }
+    }
+}
