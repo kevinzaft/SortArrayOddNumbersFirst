fae28dc3fe2281641828e01abd819e04134ace4d
Kevin Huangfu <kevinzaft@gmail.com>
2019-02-03 19:00:49 -0500

    added comments

--- CodeTesting/Program.cs
+++ CodeTesting/Program.cs
@@ -6,7 +6,9 @@ namespace CodeTesting
     {
         static void Main(string[] args)
         {
+            //initial array
             var arry = new int[6] {1,2,3,4,5,6 };
+            //sorting array
             arry = arry.GroupBy(x => x % 2 == 0).SelectMany(g => g).ToArray();
         }
     }
